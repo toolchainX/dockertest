c9c74e8925f5ff879d2195616b70c76a2d4881fe
谭林华 <linhua.tlh@alibaba-inc.com>
2016-06-28 21:01:02 +0800

    log stress test

---
+++ stress4/test.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+
+i="16000"
+
+while [ ! $i = "0" ]
+do
+	echo "$i: HAProxy image that balances between linked containers and, if launched in Docker Cloud or using Docker Compose v2, reconfigures itself when a linked cluster member redeploys, joins or leaves."
+        i=$(($i - 1))
+done
+
